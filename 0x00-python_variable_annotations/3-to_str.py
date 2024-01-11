1dcd92dfdec6511a1a0e7c398f785f4a91a41b95
shazaaly <shaza.aly@gmail.com>
2024-01-11 12:45:47 +0200

    File: 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""a script for a type-annotated function
+to_str that takes a float n as
+argument and returns the string representation of the float."""
+
+import math
+
+def to_str (n:float)-> str:
+	"""_summary_
+
+	Args:
+		n (float): float
+
+	Returns:
+		str: string
+	"""
+
+
+	return str(n)
+
