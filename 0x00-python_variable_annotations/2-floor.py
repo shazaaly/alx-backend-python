d6dc848f5c80a4fb96a30443cc94e17f3dd2e09a
shazaaly <shaza.aly@gmail.com>
2024-01-11 12:18:25 +0200

    File: 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""a script for a type-annotated function floor which takes
+a float n as argument and returns the floor of the float."""
+
+import math
+
+def floor (n:float)-> int:
+	"""_summary_
+
+	Args:
+		n (float): float
+
+	Returns:
+		floar: int
+	"""
+
+	return math.floor(n)
+
+ans = floor(3.14)
