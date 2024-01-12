1390eecb638b38c60c1575d8214da5f5570a8598
shazaaly <shaza.aly@gmail.com>
2024-01-12 22:35:43 +0200

    fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Augment the code with the correct duck-typed annotations"""
 
-from typing import List, Tuple, Sequence, Iterable
+from typing import List, Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
