2cd6e7c943b514a4fe042d0534df9c91845aeebb
shazaaly <shaza.aly@gmail.com>
2024-01-12 22:19:07 +0200

    Iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 """function with annotated parameters and
 return values with appropriate types."""
 
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Takes a list of strings and returns a list of tuples
     where each tuple is a string from the list and its length.
     """
