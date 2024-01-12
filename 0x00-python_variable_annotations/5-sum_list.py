d8ead34f8348823962ee603215c853735e5d6b95
shazaaly <shaza.aly@gmail.com>
2024-01-12 21:18:42 +0200

    pycodestyle

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,13 +8,11 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-	"""which takes a list input_list of
-	floats as argument and returns their sum as a float.
-	"""
-	sum = 0
-	for i in input_list:
-		if type(i) == float:
-			sum += i
-	return sum
-
-
+    """which takes a list input_list of
+    floats as argument and returns their sum as a float.
+    """
+    sum = 0
+    for i in input_list:
+        if type(i) == float:
+            sum += i
+    return sum
