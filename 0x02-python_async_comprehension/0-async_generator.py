6036419d0d8bbc8ffe4d4778020973de4ea37a43
shazaaly <shaza.aly@gmail.com>
2024-01-16 13:28:49 +0200

    fix 0-async_generator.py:18:28: E225 missing whitespace around operator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,7 +15,7 @@ import random
 from typing import Generator
 
 
-async def async_generator()-> Generator[float, None, None] :
+async def async_generator() -> Generator[float, None, None]:
     """
     The coroutine will loop 10 times,
     each time asynchronously wait 1 second,
