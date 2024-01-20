554647af2a932d3b92ce4478996a5e5071ea116b
shazaaly <shaza.aly@gmail.com>
2024-01-20 02:05:37 +0200

    File: 4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+     an asynchronous coroutine
+     that takes in an integer argument
+     (max_delay, with a default value of 10)
+    """
+
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays (float values).
+in ascending order without using sort() because of concurrency.
+    """
+
+    sorted_tasks = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    completed = asyncio.as_completed(tasks)
+    for task in completed:
+        result = await task
+        sorted_tasks.append(result)
+    return sorted_tasks
