57ccff12a5774855082c34ea320e3b138be3b6cb
shazaaly <shaza.aly@gmail.com>
2024-02-03 23:52:33 +0200

    Update unit tests for `utils.py`

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,9 @@
 import unittest
 import requests
 from unittest import mock
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -45,3 +46,26 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """to mock a_method. Test that when calling a_property twice,
+    """
+    def test_memoize(self):
+        """to mock a_method. Test that when calling a_property twice"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            inst = TestClass()
+            inst.a_property
+            inst.a_property
+
+            mock_method.assert_called_once()
