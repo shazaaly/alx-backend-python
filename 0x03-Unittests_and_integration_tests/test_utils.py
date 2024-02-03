4f0ca630d0fce473fa71138273725ed33050da4a
shazaaly <shaza.aly@gmail.com>
2024-02-03 11:04:47 +0200

    Add test cases for `access_nested_map` and `get_json` functions.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 """
 
 import unittest
+from unittest import mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +29,19 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, dict, path, expected, expected_key):
         with self.assertRaises(expected):
             access_nested_map(dict, path)
+
+    class TestGetJson(unittest.TestCase):
+        """ mock requests"""
+
+        @parameterized.expand([
+            {"test_url": "http://example.com", "test_payload": {"payload": True}},
+            {"test_url": "http://holberton.io", "test_payload": {"payload": False}}
+        ])
+
+        @mock.patch('utils.get_json')
+        def test_get_json(self, url, mock_get_json, expected):
+            """ test that utils.get_json returns the expected result. """
+            mock_get_json.return_value.json.return_value = expected
+            mock_get_json.assertcalledOnceWith(url)
+            result = get_json(url)
+            self.assertEqual(result, expected)
